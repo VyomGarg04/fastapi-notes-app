c2136eaf5c2a706b65638f761f7d31b0b00a8c01
VyomGarg04 <vyomgarg497@gmail.com>
2026-05-13 00:33:38 +0530

    Refactor templates using inheritance and partials

--- routes/note.py
+++ routes/note.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi import APIRouter
 from models.note import Note
@@ -11,9 +11,12 @@ from bson import ObjectId
 note = APIRouter()
 templates = Jinja2Templates(directory = "templates")
 
+notes_collection = conn.notes.notes2
+
+
 @note.get("/",response_class = HTMLResponse)
 async def get_notes(request: Request):
-    docs = conn.notes.notes2.find({})
+    docs = notes_collection.find({})
     newDocs = notesEntity(docs)
     return templates.TemplateResponse(
         request=request,
@@ -27,12 +30,12 @@ async def create_note(request: Request):
     form = await request.form()
     formDict = dict(form)
     formDict["important"] = True if formDict.get("important") == "on" else False
-    innserted_note = conn.notes.notes2.insert_one(formDict)
+    inserted_note = notes_collection.insert_one(formDict)
     return RedirectResponse(url = "/", status_code=status.HTTP_303_SEE_OTHER)
 
 @note.get("/delete/{id}",response_class = HTMLResponse)
 async def  delete_note(id: str):
-    conn.notes.notes2.delete_one({"_id": ObjectId(id)})
+    notes_collection.delete_one({"_id": ObjectId(id)})
     return RedirectResponse(url = "/", status_code=status.HTTP_303_SEE_OTHER)
 
 
@@ -40,7 +43,7 @@ async def  delete_note(id: str):
 
 @note.get("/edit/{id}",response_class = HTMLResponse)
 async def edit_note(request: Request, id: str):
-    note_found = conn.notes.notes2.find_one({"_id": ObjectId(id)})
+    note_found = notes_collection.find_one({"_id": ObjectId(id)})
     note_found = noteEntity(note_found)
     return templates.TemplateResponse(
         request=request,
@@ -57,5 +60,5 @@ async def save_edited_note(request: Request, id:str):
     form = await request.form()
     formDict = dict(form)
     formDict["important"] = True if formDict.get("important") == "on" else False
-    conn.notes.notes2.update_one({"_id": ObjectId(id)}, {"$set": formDict})
+    notes_collection.update_one({"_id": ObjectId(id)}, {"$set": formDict})
     return RedirectResponse(url = "/", status_code=status.HTTP_303_SEE_OTHER)
